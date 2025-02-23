89e6622dac766bfea32bef62119bddee0044d23e
philipnathan <philipnathann18@gmail.com>
2025-02-23 21:14:52 +0800

    Add test

--- src/main/java/com/hitachi/smartpark/repository/entity/ParkingLotEntity.java
+++ src/main/java/com/hitachi/smartpark/repository/entity/ParkingLotEntity.java
@@ -1,11 +1,17 @@
 package com.hitachi.smartpark.repository.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "PARKING_LOT")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ParkingLotEntity {
 
     @Id
--- src/main/java/com/hitachi/smartpark/repository/entity/VehicleEntity.java
+++ src/main/java/com/hitachi/smartpark/repository/entity/VehicleEntity.java
@@ -2,12 +2,18 @@ package com.hitachi.smartpark.repository.entity;
 
 import com.hitachi.smartpark.service.model.VehicleType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name = "VEHICLE")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VehicleEntity {
 
     @Id
